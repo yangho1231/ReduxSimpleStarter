673585fd5ed3bd5b96c5e28449d3f5bc43c60e25
yangho1231 <yangho1231@gmail.com>
2017-02-07 02:18:38 -0700

    Got the image and title to display

--- src/component/video_list_item.js
+++ src/component/video_list_item.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => {
-    //same as const video = props.video
-    return ( 
-        <li>{video.snippet.title}</li>
-    )
-}
+    // putting({video{) same as const video = props.video;
+    const imageUrl = video.snippet.thumbnails.default.url;
 
+    return (
+    <li className='list-group-item'>
+            <div className='video-list media'>
+                <div className='media-left'>
+                    <img className = 'media-object' src={imageUrl}/>
+                </div>
+            </div>
+            <div className='media-body'>
+                <div className='media-heading'>{video.snippet.title}</div>
+            </div>
+
+        </li>
+    );
+}
 export default VideoListItem;
