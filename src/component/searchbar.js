024b9123d63c795f33039601185a4c459a1e826d
yangho1231 <yangho1231@gmail.com>
2017-02-06 23:34:25 -0700

    Added sample of how state works in SearchBar.js

--- src/component/searchbar.js
+++ src/component/searchbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 //{ Component } same as const Component = React.Component
+//Only Class component class has state not in funcitonal class
+//All Javascript function has constructor function
+//Parent class Component you can called that by using super(props)
+//Beside constructor function this.state should be this.setState
 class SearchBar extends Component {
-    render() {
-        return <input onChange={event => console.log(event.target.value)} />;
-    }
+    constructor(props) {
+        super(props);
 
+        this.state = { term: ''};
+    }
+    render() {
+        return ( 
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value })} />
+                Value of the input: { this.state.term }
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
