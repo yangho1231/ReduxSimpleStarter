f436c01b9ed6d607c4b381c97214f7ec112a98ee
yangho1231 <yangho1231@gmail.com>
2017-02-09 01:57:20 -0700

    Added functionality to clicanoter video from list and it will appear on details page

--- src/index.js
+++ src/index.js
@@ -11,19 +11,28 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
         //Instead of ({videos: videos}) You can just put({videos})
         //Only works if key and value are identical
         YTSearch({key: API_KEY, term: 'lebron'}, (videos) => {
-            this.setState({videos})
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         })
     }
     render () {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) } 
+                    videos={this.state.videos} 
+                />
             </div>
         );
     }
